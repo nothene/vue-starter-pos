042d58a73feb33eef809183028e265975b6aebbf
nothene <kevin_huang31@yahoo.co.id>
2022-07-15 13:56:56 +0700

    add create product

---
+++ src/components/CreateProduct.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="card m-2">
+        <div class="card-body">
+            <h5 class="card-title">Create Product</h5>
+            <p class="card-text"></p>
+        </div>
+    </div>
+</template>
