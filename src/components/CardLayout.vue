0db41ff3144e8114e819e729fd2cbe206a69f2ec
nothene <kevin_huang31@yahoo.co.id>
2022-07-16 12:03:18 +0700

    add edit product

---
+++ src/components/CardLayout.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <div class="card m-2">
+        <div class="card-body">
+            <div class="lead card-title">
+                <slot name="header"></slot>
+            </div>
+            <slot name="body"></slot>                    
+        </div>
+    </div>         
+</template>
